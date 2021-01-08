4fb3caf3facc8b5bec3eecfae9d337609dace281
Mathieu Lemay <mathieu.lemay@broadsign.com>
2021-01-08 12:09:56 -0500

    Step 1: Bind to a port

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-#[allow(unused_imports)]
-use std::env;
-#[allow(unused_imports)]
-use std::fs;
-#[allow(unused_imports)]
 use std::net::TcpListener;
 
 fn main() {
-    println!("Your code goes here!");
+    let listener = match TcpListener::bind("127.0.0.1:6379") {
+        Ok(l) => l,
+        Err(e) => panic!("Unable to start listener: {:?}", e),
+    };
 
-    // Uncomment this block to pass the first stage
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    // match listener.accept() {
-    //     Ok((_socket, addr)) => println!("accepted new client: {:?}", addr),
-    //     Err(e) => println!("couldn't accept client: {:?}", e),
-    // }
+    match listener.accept() {
+        Ok((_socket, addr)) => println!("accepted new client: {:?}", addr),
+        Err(e) => println!("couldn't accept client: {:?}", e),
+    }
 }
