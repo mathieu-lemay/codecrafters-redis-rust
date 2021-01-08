65d2bbbddc02350e3ad73e5b0b65c496ca8bb960
Mathieu Lemay <mathieu.lemay@broadsign.com>
2021-01-08 12:22:56 -0500

    Step 1: Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
     let listener = match TcpListener::bind("127.0.0.1:6379") {
-        Ok(l) => l,
+        Ok(l) => {
+            println!("Listening on {:?}", l.local_addr().unwrap());
+            l
+        }
         Err(e) => panic!("Unable to start listener: {:?}", e),
     };
 
     match listener.accept() {
-        Ok((_socket, addr)) => println!("accepted new client: {:?}", addr),
+        Ok((mut socket, addr)) => {
+            println!("accepted new client: {:?}", addr);
+            socket
+                .write_all(b"+PONG\r\n")
+                .expect("Unable to write to socket");
+        }
         Err(e) => println!("couldn't accept client: {:?}", e),
     }
 }
